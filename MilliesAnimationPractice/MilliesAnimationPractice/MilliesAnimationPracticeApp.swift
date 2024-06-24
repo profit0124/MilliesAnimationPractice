76e4e60297b6ce5f93f3864ab0522954f3ede13b
profit <profit0124@gmail.com>
2024-06-24 15:06:02 +0900

    create SwiftUI Project

---
+++ MilliesAnimationPractice/MilliesAnimationPractice/MilliesAnimationPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MilliesAnimationPracticeApp.swift
+//  MilliesAnimationPractice
+//
+//  Created by Sooik Kim on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct MilliesAnimationPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
